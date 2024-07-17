122a1ad02e1c68d11514d1994bc52e8985f3d1c4
Kim Ji Yu <spbabo97@naver.com>
2024-07-17 10:07:58 +0900

    :wrench: project set

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-      },
       use: ["@svgr/webpack"],
     });
     return config;
