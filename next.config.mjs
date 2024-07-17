3638280166db7e36e7a651f2e6e3958821edca4e
Kim Ji Yu <spbabo97@naver.com>
2024-07-17 09:45:15 +0900

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
+
+export default nextConfig;
