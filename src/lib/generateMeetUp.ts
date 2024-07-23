4c2001ebe46d8b0dd1d334f73270fdff40232181
김정헌 <metronom7703@naver.com>
2024-07-23 15:28:49 +0900

    feat: 모임 생성 모달 생성
    - 모임 생성 모달 생성
    - 서버 액션 함수 분리
    - 함수 예외 처리 완료

---
+++ src/lib/generateMeetUp.ts
@@ -0,0 +1,35 @@
+"use server"
+
+import { redirect } from "next/navigation"
+
+interface ICustomResponse {
+  code: string
+  message: string
+  parameter?: string
+}
+
+const generateMeetUp = async (formData: FormData) => {
+  try {
+    const response = await fetch(
+      "https://fe-adv-project-together-dallaem.vercel.app/team5/gatherings",
+      {
+        method: "POST",
+        body: formData,
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      },
+    )
+
+    if (!response.ok) {
+      const json: ICustomResponse = await response.json()
+      throw new Error(json.message)
+    }
+
+    redirect("/")
+  } catch (error) {
+    throw new Error(error as string)
+  }
+}
+
+export default generateMeetUp
