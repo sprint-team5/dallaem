bf50d76d2b7d823abeb8e6cbccbbb9c4e8d3ff53
KimChangmin <zzangmin3@gmail.com>
2024-07-24 10:41:57 +0900

    #31 svg 폴더 위치 루트 디렉토리로 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import Arrow from "@/components/public/icon/dynamicIcon/Arrow"
+
 const Home = () => {
-  return <main>메인 페이지 초기화</main>
+  return (
+    <main>
+      메인 페이지 초기화
+      <Arrow state="defaultDown" />
+    </main>
+  )
 }
 
 export default Home
