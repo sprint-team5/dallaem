a6000a82c92d5789d84a0569011ad905ffe3b972
KimChangmin <zzangmin3@gmail.com>
2024-07-24 10:02:16 +0900

    #31 이미지 컴포넌트 레이아웃 구현

---
+++ src/components/public/img/ImgLogin.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image"
+
+import ImgLoginIMG from "@/public/img/img_login.png"
+
+interface ImgLoginProps {
+  className?: string
+}
+
+const ImgLogin = ({ className }: ImgLoginProps) => {
+  const newClassName = `${className} w-[620px] h-[513px]`
+  return <Image src={ImgLoginIMG} alt="ImgLoginIMG" className={newClassName} />
+}
+
+export default ImgLogin
