4b1a5bc4af4930f1ccb28c05cf90cf4efe7801a5
SeungHak <bsh93367@gmail.com>
2024-07-24 09:32:58 +0900

    add: 지정된 상수 추가
    - 일반적으론 리스트도 조회하지만, api가 단촐해서 이렇게 구현

---
+++ src/constants/meeting.ts
@@ -0,0 +1,8 @@
+/** 모임 장소 */
+const location = ["건대입구", "을지로3가", "홍대입구"]
+/** 모임 타입(조회) */
+const meetingTypeInquiry = ["DALLAEMFIT", "OFFICE_STRETCHING", "MINDFULNESS", "WORKATION"]
+/** 모임 타입(등록) */
+const meetingTypeRegister = ["OFFICE_STRETCHING", "MINDFULNESS", "WORKATION"]
+
+export { location, meetingTypeInquiry, meetingTypeRegister }
