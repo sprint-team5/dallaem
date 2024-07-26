3bdd4835636bcaaf31e1792e25d9148591b18639
KimChangmin <zzangmin3@gmail.com>
2024-07-26 09:11:49 +0900

    feat: textarea 수정

--- src/components/public/textarea/Textarea.tsx
+++ src/components/public/textarea/Textarea.tsx
@@ -1,5 +1,13 @@
-interface ITextareaProps {}
+const baseStyles =
+  "h-30 w-full resize-none overflow-y-auto rounded-xl bg-[#F9FAFB] px-4 py-[10px] font-medium text-[#1F2937] placeholder-[#9CA3AF]"
 
-const Textarea = ({ children }) => {}
+interface ITextareaProps {
+  className?: string
+  placeholder: string
+}
+
+const Textarea = ({ className, placeholder }: ITextareaProps) => {
+  return <textarea className={`${baseStyles} ${className}`} rows={4} placeholder={placeholder} />
+}
 
 export default Textarea
