8d5f749ea5c19e8d774159f8f5eca7d53797971c
KimChangmin <zzangmin3@gmail.com>
2024-07-26 17:01:33 +0900

    feat: gnb 완성 및 img 컴포넌트의 image 태그 사용 방식 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+"use client"
+
+import GNB from "@/components/public/gnb/GNB"
+
 const Home = () => {
-  return <main>메인 페이지 초기화</main>
+  return (
+    <main>
+      메인 페이지 초기화
+      <GNB />
+    </main>
+  )
 }
 
 export default Home
---
+++ src/components/pages/layout/LootLayout.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { usePathname } from "next/navigation"
+
+import GNB from "@/components/public/gnb/GNB"
+import { ROUTE } from "@/constants/route"
+
+interface LootLayoutProps {
+  children: React.ReactNode
+}
+
+const LootLayout = ({ children }: LootLayoutProps) => {
+  const pathname = usePathname()
+
+  // 현재 경로가 showGNBPaths에 포함되어 있는지 확인
+  function isValidRoute(path: string): path is keyof typeof ROUTE {
+    return Object.values(ROUTE).includes(path as any)
+  }
+
+  const shouldShowGNB = isValidRoute(pathname)
+
+  return (
+    <div>
+      {shouldShowGNB && <GNB />}
+      <main>{children}</main>
+    </div>
+  )
+}
+
+export default LootLayout
