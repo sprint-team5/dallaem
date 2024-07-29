353e606696e7c019ca6989013362253d9d200839
SeungHak <bsh93367@gmail.com>
2024-07-29 11:54:43 +0900

    add: spinner 컴포넌트 추가

---
+++ src/components/public/Spinner/Spinner.tsx
@@ -0,0 +1,10 @@
+import "./Spinner.scss"
+
+const Spinner = () => {
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="spinner" />
+    </div>
+  )
+}
+export default Spinner
