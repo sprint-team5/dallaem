c1d93d150990aaff11fa1877b947ed55db77bf58
KimChangmin <zzangmin3@gmail.com>
2024-07-29 15:45:06 +0900

    fix: 오타 수정

--- src/components/pages/layout/LootLayout.tsx
+++ src/components/pages/layout/LootLayout.tsx
@@ -1,6 +1,5 @@
 import { cookies } from "next/headers"
 
-import BottomFloatingBar from "@/components/public/bottomFloatingBar/BottomFloatingBar"
 import GNB from "@/components/public/gnb/GNB"
 
 interface ILootLayoutProps {
@@ -15,7 +14,6 @@ const LootLayout = ({ children }: ILootLayoutProps) => {
     <div>
       <GNB userToken={userToken} />
       <main className="min-h-[200vh] pt-[56px] md:pt-[60px]">{children}</main>
-      <BottomFloatingBar isHost />
     </div>
   )
 }
