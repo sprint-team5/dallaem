5c166cd3bb7fd081c015e4de0eabe1e3d9abceae
Kim Ji Yu <spbabo97@naver.com>
2024-08-01 15:22:25 +0900

    test : 테스트코드 수정

--- jest.config.js
+++ jest.config.js
@@ -140,7 +140,7 @@ const config = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  // setupFiles: [],
+  setupFiles: ["jest-localstorage-mock"],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],
--- src/app/wishlist/wishlist.test.tsx
+++ src/app/wishlist/wishlist.test.tsx
@@ -1,12 +1,54 @@
+import React from "react"
+
+import useWishList from "@/hooks/useWishList"
+import SvgrMock from "@mocks/svgrMock"
 import "@testing-library/jest-dom"
 import { render, screen } from "@testing-library/react"
+import dayjs from "dayjs"
 
 import WishListPage from "./page"
 
-describe("찜목록 UI 렌더링 테스트", () => {
-  test.skip("페이지가 잘 불러오는지 테스트", () => {
-    render(<WishListPage />)
+jest.mock("@/public/icon/staticIcon/dalaemfit.svg", () => {
+  return SvgrMock
+})
 
+// Mock the useWishList hook
+jest.mock("../../hooks/useWishList")
+
+const mockUseWishList = useWishList
+
+describe("찜목록 UI 렌더링 테스트", () => {
+  beforeEach(() => {
+    render(<WishListPage />)
+  })
+
+  test("페이지가 잘 불러오는지 테스트", () => {
     expect(screen.getByText("찜한 모임")).toBeInTheDocument()
+  })
+
+  test("리스트가 잘 가져와지는지 테스트", () => {
+    const mockWishlist = [
+      {
+        id: 1,
+        registrationEnd: dayjs().add(1, "day").toISOString(),
+        // other properties
+      },
+      {
+        id: 2,
+        registrationEnd: dayjs().subtract(1, "day").toISOString(),
+        // other properties
+      },
+    ]
+
+    mockUseWishList.mockReturnValue({
+      filter: {
+        type: "DALLAEMFIT",
+        sortBy: "registrationEnd",
+      },
+      setFilter: jest.fn(),
+      isLoading: false,
+      wishlist: mockWishlist,
+      onRefresh: jest.fn(),
+    })
   })
 })
