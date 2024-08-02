447ca8c1a6a95ebfffc52264300ae503f5588679
Kim Ji Yu <spbabo97@naver.com>
2024-08-02 14:44:00 +0900

    refactor : 내부 함수 외부로 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,28 +2,28 @@ import { cookies } from "next/headers"
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
+function startsWith(request: NextRequest, path: string) {
+  return request.nextUrl.pathname.startsWith(path)
+}
+
+function toRedirect(request: NextRequest, path: string) {
+  return NextResponse.redirect(new URL(path, request.url))
+}
+
 export function middleware(request: NextRequest) {
-  function startsWith(path: string) {
-    return request.nextUrl.pathname.startsWith(path)
-  }
-
-  function toRedirect(path: string) {
-    return NextResponse.redirect(new URL(path, request.url))
-  }
-
   const userToken = cookies().get("userToken")?.value
 
   const checkUserToken = request.cookies.has("userToken") || userToken
 
   if (!checkUserToken) {
-    if (startsWith("/wishlist") || startsWith("/mypage")) {
-      return toRedirect("/auth?mode=signin")
+    if (startsWith(request, "/wishlist") || startsWith(request, "/mypage")) {
+      return toRedirect(request, "/auth?mode=signin")
     }
   }
 
   if (checkUserToken) {
-    if (startsWith("/auth")) {
-      return toRedirect("/")
+    if (startsWith(request, "/auth")) {
+      return toRedirect(request, "/")
     }
   }
 
