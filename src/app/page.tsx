826f0981b5a90ae3da21214615594176615ff0f3
KimChangmin <zzangmin3@gmail.com>
2024-08-05 13:58:23 +0900

    chore: 오타 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
 import HeadContent from "@/components/pages/mainpage/HeadContent"
 
 const Home = () => {
-  return (
-    <div>
-      {" "}
-      <div className="">ss</div>
-      <HeadContent />
-    </div>
-  )
+  return <HeadContent />
 }
 
 export default Home
