975d8d0b7aaa65ff3a53401582af85e2749f58ac
SeungHak <bsh93367@gmail.com>
2024-08-07 16:59:29 +0900

    fix: 모임 상세 refetch 오류 수정
    - thx to 지유님

--- src/app/findMeeting/[slug]/page.tsx
+++ src/app/findMeeting/[slug]/page.tsx
@@ -1,15 +1,10 @@
 import getQueryClient from "@/components/app/queryClient"
 import MeetingDetail from "@/components/pages/findMeeting/MeetingCard/MeetingDetail/MeetingDetail"
 import { meetingDetailOptions } from "@/hooks/useMeetingDetail"
-import { HydrationBoundary, dehydrate } from "@tanstack/react-query"
 
 const FindMeetingDetail = async ({ params }: { params: { slug: string } }) => {
   const queryClient = getQueryClient()
   await queryClient.prefetchQuery(meetingDetailOptions(params.slug))
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <MeetingDetail id={params.slug} />
-    </HydrationBoundary>
-  )
+  return <MeetingDetail id={params.slug} />
 }
 export default FindMeetingDetail
