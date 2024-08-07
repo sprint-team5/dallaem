97df763bde19c3a295bb16b7d11ed1a5296a4fa3
Kim Ji Yu <spbabo97@naver.com>
2024-08-07 13:39:10 +0900

    bug : tailwind 버그 수정

--- src/components/public/Skeleton/MeetingCardSkeleton.tsx
+++ src/components/public/Skeleton/MeetingCardSkeleton.tsx
@@ -1,18 +1,16 @@
-import TYPE from "@/constants/skeleton"
-
 const MeetingCardSkeleton = () => {
   return (
-    <div className="relative overflow-hidden rounded-3xl border border-gray-200 md:flex">
-      <div className={`h-[156px] w-full flex-none md:w-[280px] ${TYPE.normal}`} />
+    <div className="relative mt-6 overflow-hidden rounded-3xl border border-gray-200 first:mt-0 md:flex">
+      <div className="skeleton-normal h-[156px] w-full flex-none md:w-[280px]" />
       <div className="flex flex-1 flex-col justify-between px-6 py-4">
         <div className="flex items-center justify-between">
           <div>
-            <div className={`h-3 w-40 lg:w-60 ${TYPE.full}`} />
-            <div className={`mt-2 h-3 w-20 lg:w-28 ${TYPE.full}`} />
+            <div className="skeleton-full h-3 w-40 lg:w-60" />
+            <div className="skeleton-full mt-2 h-3 w-20 lg:w-28" />
           </div>
-          <div className={`size-12 ${TYPE.full}`} />
+          <div className="skeleton-full size-12" />
         </div>
-        <div className={`mt-7 h-3 w-full md:mt-0 ${TYPE.full}`} />
+        <div className="skeleton-full mt-7 h-3 w-full md:mt-0" />
       </div>
     </div>
   )
--- src/components/public/Skeleton/ReviewSkeleton.tsx
+++ src/components/public/Skeleton/ReviewSkeleton.tsx
@@ -1,15 +1,13 @@
-import TYPE from "@/constants/skeleton"
-
 const ReviewSkeleton = ({ imageHidden }: { imageHidden?: boolean }) => {
   return (
     <div className="flex flex-col gap-6 sm:flex-row">
       {!imageHidden && (
-        <div className={`h-[156px] w-full rounded-3xl sm:w-[280px] ${TYPE.normal}`} />
+        <div className="skeleton-normal h-[156px] w-full rounded-3xl sm:w-[280px]" />
       )}
       <div className="flex-1 border-b border-dotted pb-6">
-        <div className={`h-3 w-32 ${TYPE.full}`} />
-        <div className={`mt-[10px] h-3 w-full ${TYPE.full}`} />
-        <div className={`mt-[10px] h-3 w-52 ${TYPE.full}`} />
+        <div className="skeleton-full h-3 w-32" />
+        <div className="skeleton-full mt-[10px] h-3 w-full" />
+        <div className="skeleton-full mt-[10px] h-3 w-52" />
       </div>
     </div>
   )
