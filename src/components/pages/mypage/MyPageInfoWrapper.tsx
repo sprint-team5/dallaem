52a09229640a525754c313bee21a8b3643ea3063
김정헌 <metronom7703@naver.com>
2024-08-07 14:16:07 +0900

    feat: 무한 스크롤 구현
    - useInfiniteQuery로 변경
    - queryFn 함수 수정

--- src/components/pages/mypage/MyPageInfoWrapper.tsx
+++ src/components/pages/mypage/MyPageInfoWrapper.tsx
@@ -3,13 +3,14 @@
 import Link from "next/link"
 import { useRouter } from "next/navigation"
 
-import { MouseEvent, useState } from "react"
+import { MouseEvent, useCallback, useEffect, useState } from "react"
+import { useInView } from "react-intersection-observer"
 
 import { IGetMyPageRes, IReview, fetchMyPageInfo } from "@/actions/fetchMyPageInfo"
 import Card from "@/components/public/Card/Card"
 import Review from "@/components/public/Review/Review"
 import Spinner from "@/components/public/Spinner/Spinner"
-import { useQuery } from "@tanstack/react-query"
+import { useInfiniteQuery } from "@tanstack/react-query"
 
 import ReviewStateButton from "./ReviewStateButton"
 
@@ -30,6 +31,9 @@ const MyPageInfoWrapper = ({ dataFetchingKey, onClick, reviewed }: IMyPageInfoWr
   const initialState = reviewed && true
   const router = useRouter()
   const [hasReview, setHasReview] = useState(initialState)
+  const { ref, inView } = useInView({
+    threshold: 1,
+  })
 
   let dataSort: IDataSort = { dataFetchingKey }
 
@@ -37,17 +41,25 @@ const MyPageInfoWrapper = ({ dataFetchingKey, onClick, reviewed }: IMyPageInfoWr
     dataSort = { ...dataSort, isReviewed: true }
   }
 
-  const { data, isPending } = useQuery({
+  const { data, isPending, fetchNextPage } = useInfiniteQuery({
     queryKey: ["mypage", dataSort],
-    queryFn: ({ queryKey }) => {
+    queryFn: ({ queryKey, pageParam }) => {
       const querySort = queryKey[1] as IDataSort
       const fetchingKey = querySort.dataFetchingKey
       const isReviewed = querySort.isReviewed ?? null
-      const offset = 0
+      const offset = pageParam
       const limit = 5
       return fetchMyPageInfo({ fetchingKey, offset, limit, isReviewed })
     },
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages, lastPageParam) => {
+      return lastPage.hasMore ? lastPageParam + 5 : undefined
+    },
   })
+
+  const fetchNextPageCallback = useCallback(() => {
+    return fetchNextPage
+  }, [fetchNextPage])
 
   const reviewButtonHandler = () => {
     setHasReview(!hasReview)
@@ -58,54 +70,63 @@ const MyPageInfoWrapper = ({ dataFetchingKey, onClick, reviewed }: IMyPageInfoWr
     onClick({ type: "myReview", reviewed: true })
     setHasReview(true)
   }
+
   const clickCreateReviewHandler = (e: MouseEvent, pathId: number) => {
     e.preventDefault()
     router.push(`/mypage/addReview?gatheringId=${pathId}`)
   }
 
-  if (isPending) return <Spinner />
+  useEffect(() => {
+    fetchNextPageCallback()
+  }, [inView, fetchNextPageCallback])
 
+  const dataPages = data?.pages ?? []
+
+  if (isPending) return <Spinner />
   return (
     <>
       {isMyReview && <ReviewStateButton onClick={reviewButtonHandler} hasReview={hasReview} />}
-      <div className="flex flex-col gap-6">
-        {data &&
-          data.map((item: IGetMyPageRes & IReview) => {
-            if (isMyReview && hasReview) {
+      <div className="relative flex flex-col gap-6 overflow-hidden">
+        {dataPages &&
+          dataPages.map((dataPage) => {
+            return dataPage.data.map((item: IGetMyPageRes & IReview) => {
+              if (isMyReview && hasReview) {
+                return (
+                  <Review
+                    key={item.id}
+                    score={item.score}
+                    createdAt={item.createdAt}
+                    comment={item.comment}
+                    gathering={item.Gathering}
+                    isImage={item.Gathering.image !== ""}
+                  />
+                )
+              }
               return (
-                <Review
-                  key={item.id}
-                  score={item.score}
-                  createdAt={item.createdAt}
-                  comment={item.comment}
-                  gathering={item.Gathering}
-                  isImage={item.Gathering.image !== ""}
-                />
+                <Link key={item.name} href={`/findMeeting/${item.id}`}>
+                  <Card
+                    handlerReview={(e: MouseEvent) => {
+                      clickCreateReviewHandler(e, item.id)
+                    }}
+                    handlerView={clickViewReviewHandler}
+                    teamId={item.teamId}
+                    id={item.id}
+                    name={item.name}
+                    dateTime={item.dateTime}
+                    registrationEnd={item.registrationEnd}
+                    location={item.location}
+                    participantCount={item.participantCount}
+                    image={item.image}
+                    capacity={item.capacity}
+                    isBtnHide={isMyOwnMeeting}
+                    isMy={isMyOwnMeeting || !hasReview}
+                    isReview={item.isReviewed}
+                  />
+                </Link>
               )
-            }
-            return (
-              <Link key={item.name} href={`/findMeeting/${item.id}`}>
-                <Card
-                  handlerReview={(e: MouseEvent) => {
-                    clickCreateReviewHandler(e, item.id)
-                  }}
-                  handlerView={clickViewReviewHandler}
-                  teamId={item.teamId}
-                  id={item.id}
-                  name={item.name}
-                  dateTime={item.dateTime}
-                  registrationEnd={item.registrationEnd}
-                  location={item.location}
-                  participantCount={item.participantCount}
-                  image={item.image}
-                  capacity={item.capacity}
-                  isBtnHide={isMyOwnMeeting}
-                  isMy={isMyOwnMeeting || !hasReview}
-                  isReview={item.isReviewed}
-                />
-              </Link>
-            )
+            })
           })}
+        <div ref={ref} className="h-1 w-full" />
       </div>
     </>
   )
