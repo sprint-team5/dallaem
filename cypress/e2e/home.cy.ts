eda04c95cbf7ef000290f59d013582c23d45c066
김정헌 <metronom7703@naver.com>
2024-08-12 14:05:48 +0900

    feat: home page 기본 렌더링 동작 테스트
    - pathname 확인
    - dom node 렌더링 확인
    - scroll 동작 확인

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,13 +1,19 @@
-describe("template spec", () => {
+describe("home page test", () => {
   beforeEach(() => {
     cy.visit("/")
   })
   it("should visit home and the text", () => {
     cy.get("a[href*='/']").first().click()
-    cy.get("h1").should("have.text", "당신의 관심사,5분 만에 모임으로 만들어보세요!")
+    cy.location("pathname").should("eq", "/")
+    cy.get("h1").contains("당신의 관심사")
   })
-  it("should visit login page", () => {
-    cy.get("a[href*='/auth?mode=signin'").first().click()
-    cy.get("form > span:first-child").should("have.text", "로그인")
+  it("should visit home and the text", () => {
+    cy.get("a[href*='/']").first().click()
+    cy.get("a").should("have.length", 7)
+  })
+  it("should show review after scroll", () => {
+    cy.get("a[href*='/']").first().click()
+    cy.scrollTo(0, 600)
+    cy.get("h3").contains("실제 후기입니다.")
   })
 })
