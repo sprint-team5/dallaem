3cee7ab4a04840042e53279b786a6c5af176609a
김정헌 <metronom7703@naver.com>
2024-08-13 09:37:34 +0900

    chore: 오타 수정
    - 오타 수정

--- src/components/pages/wishlist/ByeBtn.tsx
+++ src/components/pages/wishlist/ByeBtn.tsx
@@ -5,8 +5,8 @@ import React from "react"
 import Bye from "@/components/public/icon/staticIcon/Bye"
 import { IWishListData } from "@/types/wishlist/wishlist"
 
-const ByeBtn = ({ list, removeHanlder }: { list: IWishListData; removeHanlder?: any }) => {
-  const HanlderRemove = () => {
+const ByeBtn = ({ list, removeHandler }: { list: IWishListData; removeHandler?: any }) => {
+  const HandlerRemove = () => {
     const wish = localStorage.getItem("wishlist")
     if (wish) {
       const parse: IWishListData[] = JSON.parse(wish)
@@ -17,7 +17,7 @@ const ByeBtn = ({ list, removeHanlder }: { list: IWishListData; removeHanlder?: 
         parse.splice(index, 1)
       }
       localStorage.setItem("wishlist", JSON.stringify([...parse]))
-      removeHanlder(list.id)
+      removeHandler(list.id)
     }
   }
 
@@ -25,7 +25,7 @@ const ByeBtn = ({ list, removeHanlder }: { list: IWishListData; removeHanlder?: 
     <button
       aria-label="wishRemove"
       type="button"
-      onClick={HanlderRemove}
+      onClick={HandlerRemove}
       className="z-20 flex cursor-pointer items-center justify-center gap-[2px] rounded-xl bg-orange-50 px-3 py-[6px] text-orange-600 sm:absolute sm:right-6 sm:top-6 sm:size-12 sm:rounded-full sm:p-0"
     >
       <Bye />
