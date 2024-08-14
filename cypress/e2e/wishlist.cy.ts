62c3d0186a058932e62055abad5da98d71c7b7dc
김정헌 <metronom7703@naver.com>
2024-08-14 13:18:09 +0900

    feat: 찜한 목록 e2e 테스트
    - 로그인 하지 않았을 시 접속 불가
    - 찜한 목록 카운트 증가 및 감소 테스트
    - 로그인 후 찜한 목록 페이지 접근 테스트
    - 페이지 기본 렌더링 및 버튼 동작 테스트

--- cypress/e2e/wishlist.cy.ts
+++ cypress/e2e/wishlist.cy.ts
@@ -45,3 +45,40 @@ describe("add wish button test", () => {
     cy.get('[href="/wishlist"] > .relative').should("not.exist")
   })
 })
+
+describe("wishlist page test", () => {
+  beforeEach(() => {
+    cy.visit("/auth?mode=signin")
+    cy.get("#email").type("test@test.com")
+    cy.get("#password").type("testtest")
+    cy.get(".group").click()
+    cy.wait(1000)
+    cy.get('[href="/wishlist"]').click()
+  })
+  it("should access wishlist page", () => {
+    cy.get(".text-lg").should("have.text", "찜한 모임")
+    cy.contains("마감되기 전에 지금 바로 참여해보세요").should("be.visible")
+    cy.contains("아직 찜한 모임이 없어요").should("be.visible")
+  })
+  it("should change active state", () => {
+    cy.contains("워케이션").click()
+    cy.contains("워케이션").should("have.attr", "class").and("contain", "active")
+    cy.contains("지역 선택").click()
+    cy.get(":nth-child(1) > .relative.inline-flex > .absolute")
+      .should("have.attr", "aria-expanded")
+      .and("eq", "true")
+    cy.contains("건대입구").click()
+    cy.get(":nth-child(1) > .relative.inline-flex > .cursor-pointer > .flex").should(
+      "have.text",
+      "건대입구",
+    )
+    cy.contains("날짜 선택").click()
+    cy.get("[role=listbox]").should("be.visible")
+    cy.contains("마감 임박 순").click()
+    cy.get(".ml-auto > .relative > .absolute")
+      .should("have.attr", "aria-expanded")
+      .and("eq", "true")
+    cy.get('[value="dateTime"]').click()
+    cy.contains("등록순").should("be.visible")
+  })
+})
