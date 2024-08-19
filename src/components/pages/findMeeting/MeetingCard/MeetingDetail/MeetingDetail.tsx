e3c0415fd31c8e5d39e917fbb5a46f09eb420dce
Kim Ji Yu <spbabo97@naver.com>
2024-08-19 18:53:28 +0900

    feat : 상세페이지 마감 시간 구현

--- src/components/pages/findMeeting/MeetingCard/MeetingDetail/MeetingDetail.tsx
+++ src/components/pages/findMeeting/MeetingCard/MeetingDetail/MeetingDetail.tsx
@@ -13,12 +13,29 @@ import WishBtn from "@/components/pages/wishlist/WishBtn"
 import Review from "@/components/public/Review/Review"
 import { useMeetingDetail } from "@/hooks/useMeetingDetail"
 import { IMeetingData } from "@/types/meeting/meeting"
+import { msTransform } from "@/util/days"
 import { useQuery } from "@tanstack/react-query"
 import dayjs from "dayjs"
 
 import BottomBanner from "./BottomBanner"
 
 export const MeetingDetailImage = ({ data }: { data: IMeetingData }) => {
+  const dayRender = (registrationEnd: string) => {
+    if (dayjs(registrationEnd).diff(dayjs(), "day") === 7) {
+      return <>다음주 마감</>
+    }
+
+    if (dayjs(registrationEnd).diff(dayjs(), "day") > 0) {
+      return <>{dayjs(registrationEnd).diff(dayjs(), "day")}일 후 마감</>
+    }
+
+    if (dayjs(registrationEnd).diff(dayjs(), "hour") <= 24) {
+      return <>오늘 {dayjs(registrationEnd).diff(dayjs(), "hour")}시 마감</>
+    }
+
+    return null
+  }
+
   return (
     <div className="relative aspect-[19/10] h-[270px] w-1/2 overflow-hidden rounded-3xl border-2 border-gray-200 max-sm:h-auto max-sm:w-full">
       <Image
@@ -28,12 +45,10 @@ export const MeetingDetailImage = ({ data }: { data: IMeetingData }) => {
         height={270}
         className="!h-full w-full object-cover"
       />
-      {dayjs(data.registrationEnd).format("YYYY-MM-DD") === dayjs().format("YYYY-MM-DD") && (
-        <div className="absolute right-0 top-0 inline-flex items-center rounded-bl-xl bg-orange-600 px-[10px] py-[4px]">
+      {msTransform(data.registrationEnd) > dayjs().unix() && (
+        <div className="absolute right-0 top-0 inline-flex items-center rounded-bl-xl bg-primary px-[10px] py-[4px]">
           <Image src="/icon/staticIcon/clock.svg" alt="마감 임박" width={24} height={24} />
-          <span className="text-xs text-white">
-            오늘 {dayjs(data.registrationEnd).format("H")}시 마감
-          </span>
+          <span className="text-xs text-white">{dayRender(data.registrationEnd)}</span>
         </div>
       )}
     </div>
