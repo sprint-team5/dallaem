6c0b5236c70e4cd6021884f5febedd1cd37ed23e
김정헌 <metronom7703@naver.com>
2024-08-19 18:19:26 +0900

    feat: 테스트 코드 추가
    - 프로필 모달 fail test
    - 로그아웃 테스트

--- cypress/e2e/mypage.cy.ts
+++ cypress/e2e/mypage.cy.ts
@@ -7,15 +7,25 @@ describe("mypage test", () => {
   })
   it("should be able to visit mypage", () => {
     cy.getCookie("userToken").should("exist")
-    cy.contains("마이페이지").should("be.visible")
     cy.contains("내 프로필").should("be.visible")
     cy.contains("참여한 모임").should("be.visible")
     cy.contains("나의 리뷰").should("be.visible")
     cy.contains("내가 만든 모임").should("be.visible")
   })
-  it("should change user profile edit modal", () => {
+  it("should be able to access and use profile edit modal", () => {
     cy.get(".size-8 > div > img").click()
     cy.location("pathname").should("include", "edit")
     cy.contains("프로필 수정하기").should("be.visible")
+    cy.get(".bg-gray-400").as("editButton").click()
+    cy.contains("이미지를 업로드해주세요.").should("be.visible")
+    cy.get("input.mt-2").as("inputCompany").type("a")
+    cy.contains("회사 이름을 입력해주세요.").should("be.visible")
+    cy.get("button[aria-label=Close]").as("cancelButton").click()
+    cy.location("pathname").should("eq", "/mypage")
+  })
+  it("should be able to logout", () => {
+    cy.intercept("/").as("home")
+    cy.contains("로그아웃").click()
+    cy.getCookie("userToken").should("not.exist")
   })
 })
