d5ff1ed3c3159bb257d34df3a4f391d4435a2999
김정헌 <metronom7703@naver.com>
2024-08-19 13:06:28 +0900

    style: not-found 색상 수정
    - 텍스트 색상 수정
    - 버튼 색상 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,11 +5,11 @@ import ROUTE from "@/constants/route"
 const notFound = () => {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
-      <h2 className="text-5xl font-bold text-orange-600">404</h2>
+      <h2 className="text-primary text-5xl font-bold">404</h2>
       <p className="mt-2 text-xl font-medium">잘못된 페이지 접근 입니다.</p>
       <Link
         href={ROUTE.HOME}
-        className="mt-4 rounded-xl border border-orange-600 px-4 py-2 text-sm font-medium text-orange-600 transition-colors hover:bg-orange-600 hover:text-white"
+        className="border-primary text-primary hover:bg-primary mt-4 rounded-lg border px-4 py-2 text-sm font-medium transition-colors hover:text-white"
       >
         메인으로 돌아가기
       </Link>
