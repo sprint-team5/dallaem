ad0c01563e93a91217787d2953b3a6f57cb7966a
김정헌 <metronom7703@naver.com>
2024-08-19 13:08:42 +0900

    feat: 버튼 관련 ui 추가
    - 하단 라인 이동 ui 추가
    - ui 관련 padding margin 수정

--- src/components/pages/mypage/MyPageInfoTapButton.tsx
+++ src/components/pages/mypage/MyPageInfoTapButton.tsx
@@ -25,7 +25,7 @@ const MyPageInfoTapButton = ({ isActive, state, onClick }: IMyPageInfoTapButton)
     <button
       onClick={clickHandler}
       type="button"
-      className={`text-sm font-semibold text-gray-400 md:text-base lg:text-lg ${isActive ? "text-gray-900" : "text-gray-400"} whitespace-nowrap`}
+      className={`pb-2 text-sm font-semibold text-gray-400 md:text-base ${isActive ? "text-gray-900" : "text-gray-400"} whitespace-nowrap`}
     >
       {text[state]}
     </button>
