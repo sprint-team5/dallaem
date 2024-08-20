0a2a465098b4ecd3311f67ab979eceb50a31c477
KimChangmin <zzangmin3@gmail.com>
2024-08-20 15:43:30 +0900

    refactor: gnb 디자인 수정

--- src/components/public/gnb/GNB.tsx
+++ src/components/public/gnb/GNB.tsx
@@ -3,11 +3,15 @@
 import Link from "next/link"
 import { usePathname, useSearchParams } from "next/navigation"
 
+import { useRef, useState } from "react"
+
 import Logo from "@/components/public/img/Logo"
 import ROUTE from "@/constants/route"
 import useGetUserData from "@/hooks/useGetUserData"
-import { useWishCount } from "@/provider/CountProvider"
+import useOutsideClick from "@/util/useOutsideClick"
 
+import AnimatedMenu from "./components/AnimatedMenu"
+import AnimatedMenuIcon from "./components/AnimatedMenuIcon"
 import ProfileComponent from "./components/ProfileComponent"
 
 // 테일윈드 스타일
@@ -18,41 +22,34 @@ const wrapperStyles = {
   desktop: "xl:w-[1198px] xl:mx-auto",
 }
 
-const navbarStyles = {
-  default: "flex items-center justify-between",
-  mobile: "gap-3",
-  tablet: "md:gap-5",
-  desktop: "xl:gap-6",
-}
-
 const gnbStyles = {
   container:
     "fixed left-0 top-0 z-50 flex w-full items-center justify-between whitespace-nowrap border-b border-gray-400 bg-white px-4 md:px-6 xl:px-0",
   wrapper: `${wrapperStyles.default} ${wrapperStyles.mobile} ${wrapperStyles.tablet} ${wrapperStyles.desktop}`,
-  navbar: `${navbarStyles.default} ${navbarStyles.mobile} ${navbarStyles.tablet} ${navbarStyles.desktop}`,
-  navItem: "font-semibold text-[#111827]",
-  currentNavItem: "font-semibold text-orange-600",
   hoveredNavItem: "transition-all ease-in-out transform hover:scale-125 delay-[10ms] duration-150",
 }
 
 const logoStyles = `${gnbStyles.hoveredNavItem} text-orange-600 w-[60px] h-6 md:w-[70px] md:h-9`
-const navBaseStyles = `${gnbStyles.navItem} ${gnbStyles.hoveredNavItem}`
-const currentNavStyles = `${gnbStyles.currentNavItem} ${gnbStyles.hoveredNavItem}`
 
 interface IGNBProps {
   userToken: string | undefined
 }
 
 const GNB = ({ userToken }: IGNBProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+
   const { data } = useGetUserData(userToken)
-  const { wishCount } = useWishCount()
-  const isClient = typeof window !== "undefined"
 
   const isLoggedIn = Boolean(data?.name)
   const profileImg = data?.image
 
   const currentPath = usePathname()
   const searchParams = useSearchParams()
+
+  const menuRef = useRef<HTMLDivElement>(null)
+  useOutsideClick(menuRef, () => {
+    return setIsOpen(false)
+  })
 
   // 현재 경로가 showGNBPaths에 포함되어 있는지 확인
   function isValidRoute(path: string): boolean {
@@ -70,45 +67,22 @@ const GNB = ({ userToken }: IGNBProps) => {
 
   const shouldShowGNB = isValidRoute(currentPath)
 
-  const navItems = [
-    { href: ROUTE.GATHERINGS, label: "모임 찾기" },
-    { href: ROUTE.SAVE_GATHERINGS, label: "찜한 모임" },
-    { href: ROUTE.ALL_REVIEW, label: "모든 리뷰" },
-  ]
-
   if (!shouldShowGNB) return null
+
+  const navButtonClick = () => {
+    setIsOpen((prev) => {
+      return !prev
+    })
+  }
 
   return (
     <div className={gnbStyles.container}>
+      {isOpen && <AnimatedMenu menuRef={menuRef} />}
       <div className={gnbStyles.wrapper}>
-        <div className={gnbStyles.navbar}>
-          <Link href={ROUTE.HOME}>
-            <Logo state="large" className={logoStyles} />
-          </Link>
-          {navItems.map((item) => {
-            const isLabel = item.label === "찜한 모임"
-            return (
-              <Link
-                key={item.href}
-                href={item.href}
-                className={currentPath === item.href ? currentNavStyles : navBaseStyles}
-              >
-                {item.label}
-                {isClient && isLabel && wishCount > 0 && (
-                  <span
-                    className={
-                      currentPath === item.href
-                        ? "relative bottom-[1px] ml-1 rounded-full bg-orange-600 px-2 py-0 text-sm font-medium text-white"
-                        : "relative bottom-[1px] ml-1 rounded-full bg-[#111827] px-2 py-0 text-sm font-medium text-white"
-                    }
-                  >
-                    {wishCount}
-                  </span>
-                )}
-              </Link>
-            )
-          })}
-        </div>
+        <AnimatedMenuIcon onClick={navButtonClick} isOpen={isOpen} />
+        <Link href={ROUTE.HOME} className="ml-5 mr-auto">
+          <Logo state="large" className={logoStyles} />
+        </Link>
         <ProfileComponent isLoggedIn={isLoggedIn} profileImg={profileImg} />
       </div>
     </div>
