f72712636e82d275f5c9b8979a37139b498fc8bf
김정헌 <metronom7703@naver.com>
2024-08-26 14:18:19 +0900

    refactor: queryOption 분리
    - queryOptions 파일 분리

--- src/app/(MyPage)/mypage/page.tsx
+++ src/app/(MyPage)/mypage/page.tsx
@@ -1,19 +1,11 @@
-import { fetchMyPageInfo } from "@/actions/Gatherings/fetchMyPageInfo"
 import MyPageInfoTap from "@/components/pages/mypage/MyPageInfoTap"
 import ProfileBox from "@/components/public/ProfileBox"
-import LIMIT from "@/constants/limit"
+import MyPagePrefetchOption from "@/hooks/mypage/myPageQuery"
 import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query"
 
 const MyPage = async () => {
   const queryClient = new QueryClient()
-  await queryClient.prefetchQuery({
-    queryKey: ["mypage", {}],
-    queryFn: () => {
-      return fetchMyPageInfo({ offset: 0, limit: LIMIT })
-    },
-    staleTime: 1000,
-    gcTime: 1000,
-  })
+  await queryClient.prefetchQuery(MyPagePrefetchOption())
 
   return (
     <div className="flex h-full flex-col items-stretch gap-3">
